857dd13c819129c0c9fb2b464f018ead4428b975
Sara Sanchez <saritt@MacBook-Air-de-Sara.local>
2023-03-20 17:07:15 +0100

    modificaciones

--- backApiRest/src/controller/apiBooks.controller.js
+++ backApiRest/src/controller/apiBooks.controller.js
@@ -1,4 +1,5 @@
-const connection = require("../dataBase")
+const connection = require("../dataBase");
+const Book = require('../models/book');
 
 
 
@@ -43,4 +44,112 @@ function postLogin(request, response){
 }
 
 
-module.exports = {postRegister, postLogin}
+
+function getStart(request, response) {
+    let respuesta = {error: true, codigo: 200, mensaje: 'Beginning point'};
+    response.send(respuesta);
+}
+
+function getBook(request, response) {
+  let id = request.params.id;
+  let sql = "SELECT * from book WHERE Id_book = ?";
+  const params = [id];
+  let respuesta;
+
+  connection.query(sql, params, function (err, result) {
+    if (err) {
+      console.log(err);
+      response.status(500).send("No se ha podido obtener el libro");
+    } else {
+      console.log(result);
+      if (result.length>0) {
+        response.status(200).json(result);
+      } else {
+        response.status(404).send("Libro no encontrado");
+      }
+    }
+  });
+}
+
+
+function getAllBooks(request, response) {
+    let respuesta;
+    let sql = "SELECT * from book"
+
+    connection.query(sql, function (err, result) {
+      if (err) {
+        console.log(err);
+        response.status(500).send("Error al obtener todos los libros");
+      } else {
+        console.log(result);
+
+          response.status(200).json(result);
+
+      }
+    });
+}
+
+
+function postBook(request, response) {
+    let respuesta;
+    let sql = "INSERT into book (Id_user, title, type, author, price, photo) VALUES (?, ?, ?, ?, ?, ?)";
+    const {Id_user, title, type, author, price, photo} = request.body;
+    const params = [Id_user, title, type, author, price, photo];
+    connection.query(sql, params, function (err, result) {
+      if (err) {
+        console.log(err);
+        response.status(500).send("Error al agregar libro");
+      } else {
+        console.log(result);
+        if (result.insertId) {
+          response.status(201).json({ message: "Libro agregado", id: result.insertId });
+        } else {
+          response.status(500).send("Error al agregar libro");
+        }
+      }
+    });
+}
+
+
+function putBook(request, response) {
+  let sql = "UPDATE book SET Id_user=?, title=?, type=?, author=?, price=?, photo=? WHERE Id_book =?";
+  const {Id_user, title, type, author, price, photo, Id_book} = request.body;
+  const params = [ Id_user, title, type, author, price, photo, Id_book];
+  connection.query(sql, params, function (err, result) {
+      if (err) {
+        console.log(err);
+        response.status(500).send("Error al modificar libro");
+      } else {
+        console.log(result);
+        if (result.affectedRows==1) {
+          response.status(200).json({ message: "Libro modificado"});
+        } else {
+          response.status(500).send("Error al modificar libro");
+        }
+      }
+    });
+}
+
+
+function deleteBook(request, response) {
+    let id = request.params.id;
+    let sql = "DELETE FROM Book WHERE Id_book=?";
+    const params = [id];
+    connection.query(sql, params, function (err, result) {
+      if (err) {
+        console.log(err);
+        response.status(500).send("Error al eliminar libro");
+      } else {
+        console.log(result);
+        if (result.affectedRows==1) {
+          response.status(200).json({ message: "Libro borrado"});
+        } else {
+          response.status(500).send("Error al eliminar libro");
+        }
+      }
+    });
+};
+
+module.exports = {getStart, getBook, postBook, putBook, deleteBook, getAllBooks, postRegister, postLogin};
+
+
--- backApiRest/src/routes/apiBooks.routers.js
+++ backApiRest/src/routes/apiBooks.routers.js
@@ -5,4 +5,14 @@ const apiBooksCtrl = require("../controller/apiBooks.controller");
 router.post("/register", apiBooksCtrl.postRegister);
 router.post("/login", apiBooksCtrl.postLogin);
 
+
+
+
+router.get("/", apiBooksCtrl.getStart);
+router.get("/books/:id", apiBooksCtrl.getBook);
+router.get("/books", apiBooksCtrl.getAllBooks);
+router.post("/books", apiBooksCtrl.postBook);
+router.put("/books", apiBooksCtrl.putBook);
+router.delete("/books/:id", apiBooksCtrl.deleteBook);
+
 module.exports = router;
